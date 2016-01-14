01bdd8c874b8a0fd530d8f06aeb40e55c0863ff8
Danny Andrews <dannya@spiceworks.com>
2016-01-14 13:52:42 -0600

    Add mocha css

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -1,4 +1,5 @@
 /* jshint varstmt: false */
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var glob = require('glob');
 var cloneDeep = require('lodash/clone');
 var path = require('path');
@@ -8,12 +9,17 @@ var baseWebpack = require('./webpack.config.js');
 var testConfig = cloneDeep(baseWebpack);
 testConfig.entry = {
   tests: glob.sync('test/*/*spec.js'),
-  vendor: testConfig.entry.vendor.concat('mocha/mocha.js')
+  vendor: testConfig.entry.vendor.concat('mocha/mocha.js', 'mocha/mocha.css')
 };
 testConfig.output = {
   filename: '[name].js',
   path: path.resolve('test', 'dist')
 };
 testConfig.module.noParse = /mocha\.js/;
+testConfig.module.loaders.push({
+  test: /\.css$/,
+  loader: ExtractTextPlugin.extract('style', 'css')
+});
+testConfig.plugins.push(new ExtractTextPlugin('vendor', 'vendor.css'));
 
 module.exports = testConfig;
