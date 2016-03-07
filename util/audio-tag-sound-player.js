a6b721af3bb40e123a0c99945192c78fd3667e00
Danny Andrews <dannya@spiceworks.com>
2016-03-07 13:10:52 -0600

    Get working in IE

--- util/audio-tag-sound-player.js
+++ util/audio-tag-sound-player.js
@@ -1,17 +1,36 @@
 import assert from 'arg-assert';
 import i from 'icepick';
+import { range } from 'lodash';
+
+import Pool from 'app/util/resource-pool';
+
+const POOL_SIZE = 10;
 
 export default function AudioTagSoundPlayer(filepath) {
   let activePlayers = i.freeze([]);
+  let pool;
 
   function soundLoaded() {
     return Boolean(AudioTagSoundPlayer.audioCache[filepath]);
   }
 
   function setup() {
-    AudioTagSoundPlayer.audioCache =
-          i.set(AudioTagSoundPlayer.audioCache, filepath, 'fake');
-    return Promise.resolve(this);
+    const resourcePromises = range(POOL_SIZE - 1).map(num => {
+      const tag = document.createElement('audio');
+      return new Promise(resolve => {
+        tag.onloadeddata = () => resolve({id: num, tag});
+        tag.src = filepath;
+      });
+    });
+    return Promise.all(resourcePromises).then(resources => {
+      pool = Pool(resources);
+      resources.forEach(({id, tag}) => {
+        tag.onended = () => pool.free(id);
+      });
+      AudioTagSoundPlayer.audioCache =
+            i.set(AudioTagSoundPlayer.audioCache, filepath, 'fake');
+      return this;
+    });
   }
   function load() {
     assert(soundLoaded(), 'You must call "setup" before calling "load" on ' +
@@ -19,6 +38,8 @@ export default function AudioTagSoundPlayer(filepath) {
     return Promise.resolve({
       start() {
         activePlayers = i.push(activePlayers, this);
+        const {tag} = pool.busy();
+        tag.play();
       },
       stop() {}
     });
